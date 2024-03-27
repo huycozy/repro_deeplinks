7aa502a39cbe8e132bbe99c521925afa7c0f2017
Nguyen Huy <huy@nevercode.io>
2024-03-27 15:36:00 +0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+```console
+adb shell am start -a android.intent.action.VIEW -c android.intent.category.BROWSABLE -d "triage://repro.com/details"
+```
---
+++ android/app/src/main/kotlin/com/example/reproduce_issue_deeplink/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reproduce_issue_deeplink
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
